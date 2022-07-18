5b52b7a1e8353f14bcb02dee046c1111efc5d5af
joaopfbatista321 <joaopfbatista@gmail.com>
2022-07-18 16:38:00 +0100

    Revert "Add project files."
    This reverts commit fde5ca53ae8549c5727a7c957d30661e8e4e5533.

--- JFauto/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace psi_2022_oficinas.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
